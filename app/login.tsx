bb3ada17795c29ffe6ffa79d97b2b9a10b73963e
grahacr <courtney.graham@atlasschool.com>
2025-02-25 18:18:23 -0600

    login page styling complete and rendering

--- app/login.tsx
+++ app/login.tsx
@@ -1,41 +1,70 @@
-import { Text, View, StyleSheet, Pressable, TextInput } from "react-native";
+import { Text, View, StyleSheet, Pressable, TextInput, Image, Keyboard, Button } from "react-native";
 import { Link, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 
 export default function LoginPage() {
+
     const router = useRouter();
 
     return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
-        <Text style={{fontSize: 24}}>Login</Text>
+    <View style={styles.pageStyle}>
+        <Image source={require('../assets/images/logo.png')} style={styles.imageLogo} resizeMode="contain" />
+        <Text style={{fontSize: 24, color: "white", marginBottom: 15}}>Login</Text>
+
         <TextInput
-        style={styles.inputStyle}
-        placeholder="Email"
-        placeholderTextColor="#888"
-        keyboardType="email-address"
+            style={styles.inputStyle}
+            placeholder="Email"
+            keyboardType="email-address"
         />
-            <Link href="/register" replace>
-                <Text style={styles.profileText}>Create an Account</Text>
-            </Link>
-            <Pressable onPress={() => {
-                router.replace("../(tabs)/");
-            }}>
+        <TextInput
+            style={styles.inputStyle}
+            placeholder="Password"
+            keyboardType="default"
+        />
+        <Pressable onPress={() => {
+            router.replace("../(tabs)/");
+            }} style={styles.signIn}>
             <Text style={styles.profileText}>Sign In</Text>
-            </Pressable>
+        </Pressable>
+        <Link href="/register" replace>
+            <Text style={styles.profileText}>Create a new Account</Text>
+        </Link>
     </View>
     );
 }
 
 const styles = StyleSheet.create({
+    pageStyle: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#00003C",
+        padding: 20
+    },
+    imageLogo: {
+        width: 250,
+        height: 250
+    },
     profileText: {
         color: "white",
+    },
+    signIn: {
+        backgroundColor: "#1ED2AF",
+        width: "100%",
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 7,
+        marginVertical: 15,
     },
     inputStyle: {
         width: "100%",
         height: 50,
-        borderColor: "#000000",
-        borderWidth: 4,
-        padding: 10,
-        marginBottom: 20,
+        borderColor: "#1ED2AF",
+        borderWidth: 2,
+        borderRadius: 7,
+        padding: 15,
+        marginBottom: 10,
         color: "#000",
     }
   });
